acccf44816f731888bd78256244177658e562b5d
Shweta <shwetaaguptaa2208@gmail.com>
2026-07-23 22:14:51 +0530

    Remove dataset dependency

--- backend/predictor.py
+++ backend/predictor.py
@@ -6,27 +6,36 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 MODEL_PATH = PROJECT_ROOT / "ai-model" / "models" / "plant_disease_model.keras"
 
-DATASET_PATH = PROJECT_ROOT / "dataset" / "PlantVillage"
-
 IMAGE_SIZE = (224, 224)
 
-class_names = sorted(
-    folder.name
-    for folder in DATASET_PATH.iterdir()
-    if folder.is_dir()
-)
+# Class names used during training
+class_names = [
+    "Pepper__bell___Bacterial_spot",
+    "Pepper__bell___healthy",
+    "Potato___Early_blight",
+    "Potato___Late_blight",
+    "Potato___healthy",
+    "Tomato_Bacterial_spot",
+    "Tomato_Early_blight",
+    "Tomato_Late_blight",
+    "Tomato_Leaf_Mold",
+    "Tomato_Septoria_leaf_spot",
+    "Tomato_Spider_mites_Two_spotted_spider_mite",
+    "Tomato__Target_Spot",
+    "Tomato__Tomato_YellowLeaf__Curl_Virus",
+    "Tomato__Tomato_mosaic_virus",
+    "Tomato_healthy"
+]
 
 model = tf.keras.models.load_model(MODEL_PATH)
 
 def predict(image):
-
     image = tf.keras.utils.img_to_array(image)
     image = tf.expand_dims(image, axis=0)
 
     prediction = model.predict(image, verbose=0)
 
     predicted_index = np.argmax(prediction)
-
     confidence = float(np.max(prediction) * 100)
 
     return {
